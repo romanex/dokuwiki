6ff9b3fe7ac18df551a8b491570380f86d8fb866
root <root@ip-172-31-35-78.eu-central-1.compute.internal>
2020-07-09 11:11:49 +0000

    My initial

---
+++ dokuwiki/conf/users.auth.php
@@ -0,0 +1,13 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,separated
+
+
+superuser:$1$r0ld5DcI$R/4mnJ88v5S0cGR4OUKvu.:Full Name:user@example.com:admin,user
+spoc:$1$HoC7iKf6$8U7aY8y26SMgJP8vgSUNy/:Spoc admin:it@spoc.com.ua:admin
---
+++ dokuwiki/data/cache/a/afd7a17e451878ad20397f360b22325f.js
@@ -0,0 +1,1 @@
+SimpleSVG._loaderCallback({"prefix":"mdi","icons":{"home":{"body":"<path d=\"M10 20v-6h4v6h5v-8h3L12 3L2 12h3v8h5z\" fill=\"currentColor\"\/>"},"file-plus":{"body":"<path d=\"M13 9h5.5L13 3.5V9M6 2h8l6 6v12a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V4c0-1.11.89-2 2-2m5 13v-3H9v3H6v2h3v3h2v-3h3v-2h-3z\" fill=\"currentColor\"\/>"}},"aliases":[],"width":24,"height":24});
