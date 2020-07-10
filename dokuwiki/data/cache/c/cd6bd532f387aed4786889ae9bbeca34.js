ca7dd1df0e9307f0259d1c8f6257000f8fe47a7c
ceazi <ceazi@outlook.com>
2020-07-10 17:57:19 +0300

    added 100720

---
+++ dokuwiki/data/cache/c/cd6bd532f387aed4786889ae9bbeca34.js
@@ -0,0 +1,1 @@
+SimpleSVG._loaderCallback({"prefix":"mdi","icons":{"alert":{"body":"<path d=\"M13 14h-2v-4h2m0 8h-2v-2h2M1 21h22L12 2L1 21z\" fill=\"currentColor\"\/>"}},"aliases":[],"width":24,"height":24});
